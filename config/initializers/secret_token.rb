6936c8644bd3000b57c444e6fbef67b97f259c51
Kevin Krauss <earlkrauss@gmail.com>
2011-05-31 08:43:03 -0700

    new Rails app generated by rails_apps_composer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollageCreator::Application.config.secret_token = '16fdb62937ddf53a52ca312537b62232fac1ad7b371f05a505bf40933b59357aae9b27482905472f279f14f6756f9ce0f911c0a802a1c9dac0cf9a0c91f3c67b'
